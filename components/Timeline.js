10cd5a50b27b7e28746ed8d7d738599c676eba34
Hans Sebastian Tirtaputra <sebastian_hans@hotmail.com>
2020-05-18 13:46:58 +0800

    Included react-native-draggable libray for draggable components

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { View, Text } from "react-native";
+import Draggable from 'react-native-draggable'; // Library to allow draggable objects, for better UI
 
 // might use tab navigator and define a static property
 const Timeline = () => {
   return (
     <View>
-      <Text>Timeline</Text>
-      <Text>Timeline</Text>
-      <Text>Timeline</Text>
-      <Text>Timeline</Text>
+      <Draggable x={75} y={100} renderSize={56} renderColor='black' renderText='A' isCircle shouldReverse onShortPressRelease={() => alert('touched!!')} />
+      <Draggable x={200} y={300} renderColor='red' renderText='B' />
+      <Draggable />
+      <Draggable x={50} y={50}>
+        <Text>Drag me</Text>
+      </Draggable>
     </View>
   );
 };
