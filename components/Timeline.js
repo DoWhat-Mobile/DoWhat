b2e70d8c7ff278364ae5bcad5a0a7a42ecc22fe7
Hans Sebastian Tirtaputra <sebastian_hans@hotmail.com>
2020-05-18 15:28:34 +0800

    Added algo to determine time from drag

--- components/Timeline.js
+++ components/Timeline.js
@@ -14,7 +14,8 @@ class Timeline extends React.Component {
   startTime = (event, gestureState) => {
     const initY = 48; // At Y.coord = 48, represents starting time: 0800hrs 
     const curY = gestureState.moveY;
-    const time = Math.floor((curY - initY) / 16) * 0.5; // Time in hrs,
+    const time = 8 + (Math.floor((curY - initY) / 16) * 0.5); // Time in hrs,
+    alert(time)
   }
 
   /**
@@ -23,6 +24,8 @@ class Timeline extends React.Component {
   endTime = (event, gestureState) => {
     const initY = 560; // End time: 2359hrs
     const curY = gestureState.moveY;
+    const time = 24 - (Math.ceil((initY - curY) / 16) * 0.5);
+    alert(time)
   }
 
   render() {
