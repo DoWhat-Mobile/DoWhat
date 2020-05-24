2eeecfbc2358af8e204ec5424fa33846aa584321
Hans Sebastian Tirtaputra <sebastian_hans@hotmail.com>
2020-05-24 18:46:29 +0800

    Added font awesome library, some styling for SyncGoogleCalendar page

--- components/SyncGoogleCalendar.js
+++ components/SyncGoogleCalendar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text, AsyncStorage, Button, StyleSheet } from "react-native";
 import { connect } from "react-redux";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 
 /**
  * Page for first step of application flow: Uploading of google calendar to get free timings
@@ -10,7 +12,16 @@ import { connect } from "react-redux";
 class SyncGoogleCalendar extends React.Component {
     render() {
         return (
-            <View>
+            <View style={style.container}>
+                <View style={style.calendar}>
+                    <FontAwesomeIcon icon={faCalendarAlt} size={80} />
+                </View>
+                <View>
+                    <Text style={style.header}>Sync Google Calendar</Text>
+                    <Text style={style.subHeader}>
+                        Upload your Google calendar for automated planning
+                    </Text>
+                </View>
                 <Button title="Skip"
                     onPress={() => this.props.navigation.navigate("Timeline")} />
             </View>
@@ -27,5 +38,18 @@ const style = StyleSheet.create({
         flexDirection: 'column',
         alignItems: "center"
     },
+    calendar: {
+        marginBottom: 50,
+    },
+    header: {
+        fontSize: 32,
+        color: '#7385d9',
+        textAlign: "center",
+    },
+    subHeader: {
+        fontSize: 16,
+        color: '#7385d9',
+        textAlign: "center",
+    }
 
 });
