28bf43e45ca4d459e360f02b8331b23a1176a75f
Hans Sebastian Tirtaputra <sebastian_hans@hotmail.com>
2020-06-12 17:03:58 +0800

    Fixed merge conflict

--- reusable-functions/GoogleCalendarInvite.js
+++ reusable-functions/GoogleCalendarInvite.js
@@ -3,17 +3,12 @@
  * Calendar invite.
  */
 const firebase = require('firebase');
-<<<<<<< HEAD
 import * as AppAuth from "expo-app-auth";
 import {
     OAuthConfig,
 } from "../reusable-functions/google_authentication_functions";
 
 export const handleProcess = (formattedData, timingsArray) => {
-=======
-
-export const handleProcess = (formattedData) => {
->>>>>>> c6f344264467b10cd5bd20050e870090101e796f
     try {
         const userId = firebase.auth().currentUser.uid;
         firebase
@@ -23,7 +18,6 @@ export const handleProcess = (formattedData) => {
             .then((snapshot) => {
                 const userData = snapshot.val();
                 const attendees = userData.all_attendees; // Object with all attendees
-<<<<<<< HEAD
                 const selectedDate = userData.selected_date; // From date selection component
                 const userEmail = userData.gmail;
                 const accessToken = [userData.access_token,
@@ -39,20 +33,12 @@ export const handleProcess = (formattedData) => {
                     userEmail,
                     accessToken);
             })
-=======
-                return formatAttendeeEmails(attendees);
-            })
-            .then(formattedAttendeeEmails => {
-                createGcalEventAndSendInvite(formattedAttendeeEmails, formattedData);
-            });
->>>>>>> c6f344264467b10cd5bd20050e870090101e796f
 
     } catch (e) {
         console.log(e);
     }
 }
 
-<<<<<<< HEAD
 
 const formatRequestAndMakeAPICall = async (allFormattedEmails, allEvents, timingsArray,
     selectedDate, userEmail, token) => {
@@ -129,19 +115,12 @@ const getValidAccessToken = async (token, tokenExpiryDate, refreshToken) => {
 const formatAttendeeEmails = (attendees) => {
     if (attendees == undefined) { // No attendees joining
         return []; // Means no attendees are free to join the scheduled events
-=======
-// Change to format usable with Gcal Event insert API.
-const formatAttendeeEmails = (attendees) => {
-    if (attendees == undefined) { // No attendees joining
-        return { attendees: [] }; // Means no attendees are free to join the scheduled events
->>>>>>> c6f344264467b10cd5bd20050e870090101e796f
     }
 
     var allFormattedEmails = [];
 
     for (var email in attendees) {
         const modifiedEmail = email.replace(/\@/g, '.') + '@gmail.com';
-<<<<<<< HEAD
         const formattedEmail = { 'email': modifiedEmail };
         allFormattedEmails.push(formattedEmail);
     }
@@ -162,16 +141,6 @@ const formatDate = (date) => {
 // Format date and hour to make it compatible with google API call
 const formatTime = (date, hour) => {
     return date + 'T' + hour + ':00:00+08:00'
-=======
-        const formattedEmail = { 'email': modifiedEmail }
-        allFormattedEmails.push(formattedEmail);
-    }
-
-    return { attendees: allFormattedEmails };
-}
-
-const createGcalEventAndSendInvite = (allFormattedEmails, allEvents) => {
->>>>>>> c6f344264467b10cd5bd20050e870090101e796f
 }
 
 const resetAllAttendeeData = () => {
@@ -182,90 +151,8 @@ export const formatEventsData = (data) => {
     var allEventDetails = [];
     data.forEach(event => {
         const title = event.title;
-<<<<<<< HEAD
         // const time = event.time; // Event time now gotten from timingsArray from finalized comp
         allEventDetails.push({ eventTitle: title });
     })
     return allEventDetails;
 }
-=======
-        const time = event.time;
-        allEventDetails.push({ eventTitle: title, eventTime: time });
-    })
-    return allEventDetails;
-}
-/*
-Array [
-  Object {
-    "description": Object {
-      "$$typeof": Symbol(react.element),
-      "_owner": FiberNode {
-        "tag": 0,
-        "key": null,
-        "type": [Function Finalized],
-      },
-      "_store": Object {},
-      "key": null,
-      "props": Object {
-        "children": Object {
-          "$$typeof": Symbol(react.element),
-          "_owner": FiberNode {
-            "tag": 0,
-            "key": null,
-            "type": [Function Finalized],
-          },
-          "_store": Object {},
-          "key": null,
-          "props": Object {
-            "children": Array [
-              "1 Empress Place",
-              " ",
-              "
-
-",
-              " ",
-              "The Asian Civilisations Museum is devoted to exploring the rich artistic heritage of Asia, especially the ancestral cultures of Singaporeans.",
-            ],
-          },
-          "ref": null,
-          "type": Object {
-            "$$typeof": Symbol(react.forward_ref),
-            "displayName": "Text",
-            "propTypes": Object {
-              "accessible": [Function bound checkType],
-              "adjustsFontSizeToFit": [Function bound checkType],
-              "allowFontScaling": [Function bound checkType],
-              "dataDetectorType": [Function bound checkType],
-              "disabled": [Function bound checkType],
-              "ellipsizeMode": [Function bound checkType],
-              "maxFontSizeMultiplier": [Function bound checkType],
-              "minimumFontScale": [Function bound checkType],
-              "nativeID": [Function bound checkType],
-              "numberOfLines": [Function bound checkType],
-              "onLayout": [Function bound checkType],
-              "onLongPress": [Function bound checkType],
-              "onPress": [Function bound checkType],
-              "pressRetentionOffset": [Function bound checkType],
-              "selectable": [Function bound checkType],
-              "selectionColor": [Function bound colorPropType],
-              "style": [Function anonymous],
-              "suppressHighlighting": [Function bound checkType],
-              "testID": [Function bound checkType],
-              "textBreakStrategy": [Function bound checkType],
-            },
-            "render": [Function Text],
-          },
-        },
-        "numberOfLines": 4,
-        "renderRevealedFooter": [Function renderRevealedFooter],
-        "renderTruncatedFooter": [Function renderTruncatedFooter],
-      },
-      "ref": null,
-      "type": [Function ReadMore],
-    },
-    "time": "13:00",
-    "title": "Asian Civilisations Museum",
-  },
-]
-*/
->>>>>>> c6f344264467b10cd5bd20050e870090101e796f
