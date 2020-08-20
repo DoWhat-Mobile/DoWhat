c77ef01fdc8d6893e026e1a5ae2141e15f970370
RuiFengg <ruifeng.chang98@hotmail.com>
2020-08-20 13:30:22 +0800

    home feed event card styling

--- components/home/FeedEventCard.js
+++ components/home/FeedEventCard.js
@@ -6,6 +6,7 @@ import {
     SectionList,
     ActivityIndicator,
     Image,
+    ImageBackground,
     FlatList,
     TouchableOpacity,
     Dimensions,
@@ -79,7 +80,7 @@ const FeedEventCard = ({
     };
 
     var imageURI = event[0].imageURL;
-    const eventRatings = event[1] + "/5";
+    const eventRatings = event[1];
 
     // If imageURI is a code, convert it to URI using TIH API
     if (imageURI.substring(0, 5) != "https") {
@@ -91,108 +92,214 @@ const FeedEventCard = ({
     }
 
     return (
-        <View>
-            <View
-                style={{
-                    flex: 1,
-                    width: Dimensions.get("window").width,
-                    marginVertical: 20,
-                    backgroundColor: "white",
-                    elevation: 5,
-                }}
-            >
-                <Image
-                    source={{ uri: imageURI }}
-                    style={{
-                        height: isEventFood ? 100 : 210,
-                        width: "100%",
-                    }}
-                />
-
-                <Text
-                    style={{
-                        fontSize: 18,
-                        fontWeight: "bold",
-                        paddingTop: 20,
-                        paddingHorizontal: 10,
-                        borderTopWidth: 0.5,
-                    }}
-                >
-                    {event[0].title}
-                </Text>
-                <View
-                    style={{
-                        flexDirection: "row",
-                        justifyContent: "space-between",
-                        paddingHorizontal: 10,
-                        marginBottom: 10,
-                    }}
-                >
-                    <View style={{ flexDirection: "row" }}>
-                        <MaterialCommunityIcons
-                            name="star"
-                            color={"#1d3557"}
-                            size={24}
-                        />
+        <ImageBackground
+            source={{ uri: imageURI }}
+            style={{
+                height: 210,
+                margin: 10,
+            }}
+            imageStyle={{ borderRadius: 10 }}
+        >
+            <View style={styles.container}>
+                <View style={styles.body}>
+                    <View style={styles.titleContainer}>
                         <Text
+                            numberOfLines={1}
+                            ellipsizeMode={"tail"}
+                            style={styles.title}
+                        >
+                            {event[0].title}
+                        </Text>
+                        <View
                             style={{
-                                fontSize: 16,
-                                color: "#1d3557",
-                                marginTop: 2,
+                                flexDirection: "row",
+                                alignItems: "center",
+                                marginLeft: 10,
                             }}
                         >
-                            {" "}
-                            {eventRatings}
-                        </Text>
+                            <MaterialCommunityIcons
+                                name="thumb-up"
+                                size={24}
+                                color="white"
+                            />
+                            <Text
+                                style={{
+                                    fontSize: 19,
+                                    color: "white",
+                                    fontWeight: "bold",
+                                    marginHorizontal: 5,
+                                }}
+                            >
+                                {" "}
+                                {eventRatings}
+                            </Text>
+                        </View>
+                        <TouchableOpacity
+                            disabled={sectionTitle == "favourites"}
+                            onPress={() =>
+                                handleAddToFavourites(
+                                    event,
+                                    sectionTitle,
+                                    index,
+                                    foodIndex
+                                )
+                            }
+                        >
+                            {isEventFavourited ? (
+                                <MaterialCommunityIcons
+                                    name="heart"
+                                    color={COLORS.orange}
+                                    size={24}
+                                />
+                            ) : (
+                                <MaterialCommunityIcons
+                                    name="heart-outline"
+                                    color={"white"}
+                                    size={24}
+                                />
+                            )}
+                        </TouchableOpacity>
                     </View>
-                    <TouchableOpacity
-                        disabled={sectionTitle == "favourites"}
-                        onPress={() =>
-                            handleAddToFavourites(
-                                event,
-                                sectionTitle,
-                                index,
-                                foodIndex
-                            )
-                        }
-                    >
-                        {isEventFavourited ? (
-                            <MaterialCommunityIcons
-                                name="heart"
-                                color={COLORS.orange}
-                                size={24}
-                            />
-                        ) : (
-                            <MaterialCommunityIcons
-                                name="heart-outline"
-                                color={"black"}
-                                size={24}
-                            />
-                        )}
-                    </TouchableOpacity>
-                </View>
-
-                <View style={{ marginLeft: 10 }}>
-                    <ReadMore
+                    <Text
+                        style={{ color: "white", fontSize: 13, width: 350 }}
                         numberOfLines={1}
-                        renderTruncatedFooter={renderTruncatedFooter}
-                        renderRevealedFooter={renderRevealedFooter}
+                        ellipsizeMode={"tail"}
                     >
-                        <Text
-                            style={{
-                                marginHorizontal: 10,
-                                marginVertical: 5,
-                                fontSize: 16,
-                            }}
-                        >
-                            {event[0].description}
-                            {"\n"}
-                        </Text>
-                    </ReadMore>
+                        {event[0].description}
+                    </Text>
                 </View>
             </View>
-        </View>
+        </ImageBackground>
+        // <View>
+        //     <View
+        //         style={{
+        //             flex: 1,
+        //             width: Dimensions.get("window").width,
+        //             marginVertical: 20,
+        //             backgroundColor: "white",
+        //             elevation: 5,
+        //         }}
+        //     >
+        //         <Image
+        //             source={{ uri: imageURI }}
+        //             style={{
+        //                 height: isEventFood ? 100 : 210,
+        //                 width: "100%",
+        //             }}
+        //         />
+
+        //         <Text
+        //             style={{
+        //                 fontSize: 18,
+        //                 fontWeight: "bold",
+        //                 paddingTop: 20,
+        //                 paddingHorizontal: 10,
+        //                 borderTopWidth: 0.5,
+        //             }}
+        //         >
+        //             {event[0].title}
+        //         </Text>
+        //         <View
+        //             style={{
+        //                 flexDirection: "row",
+        //                 justifyContent: "space-between",
+        //                 paddingHorizontal: 10,
+        //                 marginBottom: 10,
+        //             }}
+        //         >
+        //             <View style={{ flexDirection: "row" }}>
+        //                 <MaterialCommunityIcons
+        //                     name="star"
+        //                     color={"#1d3557"}
+        //                     size={24}
+        //                 />
+        //                 <Text
+        //                     style={{
+        //                         fontSize: 16,
+        //                         color: "#1d3557",
+        //                         marginTop: 2,
+        //                     }}
+        //                 >
+        //                     {" "}
+        //                     {eventRatings}
+        //                 </Text>
+        //             </View>
+        //             <TouchableOpacity
+        //                 disabled={sectionTitle == "favourites"}
+        //                 onPress={() =>
+        //                     handleAddToFavourites(
+        //                         event,
+        //                         sectionTitle,
+        //                         index,
+        //                         foodIndex
+        //                     )
+        //                 }
+        //             >
+        //                 {isEventFavourited ? (
+        //                     <MaterialCommunityIcons
+        //                         name="heart"
+        //                         color={COLORS.orange}
+        //                         size={24}
+        //                     />
+        //                 ) : (
+        //                     <MaterialCommunityIcons
+        //                         name="heart-outline"
+        //                         color={"black"}
+        //                         size={24}
+        //                     />
+        //                 )}
+        //             </TouchableOpacity>
+        //         </View>
+
+        //         <View style={{ marginLeft: 10 }}>
+        //             <ReadMore
+        //                 numberOfLines={1}
+        //                 renderTruncatedFooter={renderTruncatedFooter}
+        //                 renderRevealedFooter={renderRevealedFooter}
+        //             >
+        //                 <Text
+        //                     style={{
+        //                         marginHorizontal: 10,
+        //                         marginVertical: 5,
+        //                         fontSize: 16,
+        //                     }}
+        //                 >
+        //                     {event[0].description}
+        //                     {"\n"}
+        //                 </Text>
+        //             </ReadMore>
+        //         </View>
+        //     </View>
+        // </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgba(0,0,0,0.15)",
+        justifyContent: "flex-end",
+        borderRadius: 10,
+    },
+    body: {
+        flex: 1,
+        backgroundColor: "rgba(0,0,0,0.15)",
+        justifyContent: "flex-end",
+        borderRadius: 10,
+        padding: 15,
+    },
+    titleContainer: {
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        marginBottom: 5,
+    },
+    title: {
+        width: 250,
+        fontSize: 22,
+        fontWeight: "bold",
+        color: "white",
+    },
+});
 
 export default FeedEventCard;
