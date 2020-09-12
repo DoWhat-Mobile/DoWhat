f99a62766674211d8e84c52ada7ff9463bf020e0
hansebastian <sebastian_hans@hotmail.com>
2020-09-12 15:00:54 +0800

    Change visual assets of empty state screens

--- components/home/FriendRequestModal.js
+++ components/home/FriendRequestModal.js
@@ -1,322 +1,351 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import {
-    View, Text, StyleSheet, TouchableOpacity,
-    Dimensions, SectionList, Image, ActivityIndicator
-} from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+	View,
+	Text,
+	StyleSheet,
+	TouchableOpacity,
+	Dimensions,
+	SectionList,
+	Image,
+	ActivityIndicator,
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import { connect } from 'react-redux';
 import { removeFriend, findFriends } from '../../actions/friends_actions';
 import firebase from '../../database/firebase';
 import { Avatar } from 'react-native-elements';
 
 const FriendRequestModal = ({ userID, onClose, currUserProfilePicture }) => {
-    useEffect(() => {
-        showFriendRequests();
-    }, [])
+	useEffect(() => {
+		showFriendRequests();
+	}, []);
 
-    const [currUserName, setCurrUserName] = useState('')
-    const [friendRequests, setFriendRequests] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+	const [currUserName, setCurrUserName] = useState('');
+	const [friendRequests, setFriendRequests] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-    const showFriendRequests = () => {
-        firebase.database()
-            .ref('users/' + userID) // Current user using the app
-            .once('value')
-            .then((snapshot) => {
-                const user = snapshot.val();
-                setCurrUserName(user.first_name + '_' + user.last_name); // For identification when adding friend request to Firebase
-                if (user.hasOwnProperty('friends')) {
-                    if (user.friends.hasOwnProperty('requests')) {
-                        const allFriendRequests = user.friends.requests;
-                        addToState(allFriendRequests);
+	const showFriendRequests = () => {
+		firebase
+			.database()
+			.ref('users/' + userID) // Current user using the app
+			.once('value')
+			.then((snapshot) => {
+				const user = snapshot.val();
+				setCurrUserName(user.first_name + '_' + user.last_name); // For identification when adding friend request to Firebase
+				if (user.hasOwnProperty('friends')) {
+					if (user.friends.hasOwnProperty('requests')) {
+						const allFriendRequests = user.friends.requests;
+						addToState(allFriendRequests);
+					}
+				}
+				setIsLoading(false);
+			});
+	};
 
-                    }
-                }
-                setIsLoading(false);
-            }
-            )
-    }
+	// Add all friend requests to component state
+	const addToState = (allFriendRequests) => {
+		var friends = [];
+		for (var user in allFriendRequests) {
+			// [name, userID, profilePicURL]
+			const formattedUser = [
+				user,
+				allFriendRequests[user].firebase_id,
+				allFriendRequests[user].picture_url,
+			];
+			friends.push(formattedUser);
+		}
+		setFriendRequests([...friends]);
+	};
 
-    // Add all friend requests to component state
-    const addToState = (allFriendRequests) => {
-        var friends = [];
-        for (var user in allFriendRequests) {
-            // [name, userID, profilePicURL]
-            const formattedUser = [user, allFriendRequests[user].firebase_id,
-                allFriendRequests[user].picture_url];
-            friends.push(formattedUser);
+	// Update component state
+	const removeFromFriendList = (friendID) => {
+		const currFriends = [...friendRequests];
+		const newState = currFriends.filter((friend) => friend[1] !== friendID); // friend -> [name, userID]
+		setFriendRequests([...newState]);
+	};
 
-        }
-        setFriendRequests([...friends]);
-    }
+	const acceptFriendRequest = (name, friendID, pictureURL) => {
+		// Update from Firebase
+		let database = firebase.database();
 
-    // Update component state
-    const removeFromFriendList = (friendID) => {
-        const currFriends = [...friendRequests];
-        const newState = currFriends.filter(friend => friend[1] !== friendID); // friend -> [name, userID]
-        setFriendRequests([...newState]);
-    }
+		var updates = {};
+		updates['users/' + userID + '/friends/accepted/' + name] = {
+			firebase_id: friendID,
+			picture_url: pictureURL,
+		};
+		updates['users/' + friendID + '/friends/accepted/' + currUserName] = {
+			firebase_id: userID,
+			picture_url: currUserProfilePicture,
+		};
 
-    const acceptFriendRequest = (name, friendID, pictureURL) => {
-        // Update from Firebase
-        let database = firebase.database();
+		// Accept friend, update both users since friendship goes both ways
+		database.ref().update(updates); // Perform simultanoues update for 2 locations in Firebase
 
-        var updates = {};
-        updates['users/' + userID + '/friends/accepted/' + name] = {
-            firebase_id: friendID,
-            picture_url: pictureURL
-        };
-        updates['users/' + friendID + '/friends/accepted/' + currUserName] = {
-            firebase_id: userID,
-            picture_url: currUserProfilePicture
-        };
+		// Update Firebase
+		database
+			.ref('users/' + userID) // UserID from redux state
+			.child('friends')
+			.child('requests/' + name) // Remove friend from requested friends
+			.remove();
 
-        // Accept friend, update both users since friendship goes both ways
-        database.ref().update(updates) // Perform simultanoues update for 2 locations in Firebase
+		removeFromFriendList(friendID);
+	};
 
-        // Update Firebase 
-        database.ref("users/" + userID) // UserID from redux state
-            .child('friends')
-            .child('requests/' + name) // Remove friend from requested friends
-            .remove()
+	const rejectFriendRequest = (name, friendID) => {
+		// Update from Firebase
+		let database = firebase.database();
+		const status = {};
+		status[name] = friendID;
 
-        removeFromFriendList(friendID);
-    }
+		// Accept friend
+		database
+			.ref('users/' + userID) // Look at current user's Firebase nod
+			.child('friends')
+			.child('rejected') // Add friend to the list of accepted friends
+			.update(status);
 
-    const rejectFriendRequest = (name, friendID) => {
-        // Update from Firebase
-        let database = firebase.database();
-        const status = {};
-        status[name] = friendID;
+		// Update Firebase
+		database
+			.ref('users/' + userID) // UserID from redux state
+			.child('friends')
+			.child('requests/' + name) // Remove friend from requested friends
+			.remove();
 
-        // Accept friend
-        database.ref("users/" + userID) // Look at current user's Firebase nod 
-            .child('friends')
-            .child('rejected') // Add friend to the list of accepted friends
-            .update(status)
+		removeFromFriendList(friendID);
+	};
 
-        // Update Firebase
-        database.ref("users/" + userID) // UserID from redux state
-            .child('friends')
-            .child('requests/' + name) // Remove friend from requested friends
-            .remove()
+	// Show friends that user has already accepted
+	const renderFriends = (name, userID, pictureURL) => {
+		console.log('Stacks');
+		return (
+			<View style={styles.friend}>
+				<View style={{ margin: 5, marginTop: 10 }}>
+					<Avatar
+						rounded
+						source={{
+							uri: pictureURL,
+						}}
+						size={50}
+					/>
+				</View>
 
-        removeFromFriendList(friendID);
-    }
+				<View style={{ flex: 1, justifyContent: 'center', flexDirection: 'column' }}>
+					<Text
+						style={{
+							marginLeft: 10,
+							fontSize: 16,
+							fontWeight: '800',
+						}}
+					>
+						{name.replace(/_/g, ' ')}
+					</Text>
+					<Text
+						style={{ color: 'grey', fontSize: 14, fontWeight: '200', marginLeft: 10 }}
+					>
+						Sent a friend request to you
+					</Text>
 
-    // Show friends that user has already accepted
-    const renderFriends = (name, userID, pictureURL) => {
-        console.log("Stacks")
-        return (
-            <View style={styles.friend}>
-                <View style={{ margin: 5, marginTop: 10 }}>
-                    <Avatar
-                        rounded
-                        source={{
-                            uri: pictureURL
-                        }}
-                        size={50}
-                    />
-                </View>
+					<View style={styles.buttonGroup}>
+						<TouchableOpacity
+							style={[
+								styles.icon,
+								{
+									borderTopColor: '#7AB3EF',
+									borderEndColor: '#6EE2E9',
+									borderLeftColor: '#DED8DE',
+									borderStartColor: '#D6A5D4',
+									borderRightColor: '#DED8DE',
+									borderBottomColor: '#4ACFEA',
+								},
+							]}
+							onPress={() => acceptFriendRequest(name, userID, pictureURL)}
+						>
+							<Text>Accept</Text>
+						</TouchableOpacity>
+						<TouchableOpacity
+							style={[
+								styles.icon,
+								{
+									marginLeft: 20,
+									borderTopColor: '#D6A5D4',
+									borderEndColor: '#A49CF8',
+									borderLeftColor: '#A49CF8',
+									borderStartColor: '#D6A5D4',
+									borderRightColor: '#DED8DE',
+									borderBottomColor: '#D6A5D4',
+								},
+							]}
+							onPress={() => rejectFriendRequest(name, userID)}
+						>
+							<Text>Decline</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
+			</View>
+		);
+	};
 
-                <View style={{ flex: 1, justifyContent: 'center', flexDirection: 'column' }}>
-                    <Text style={{
-                        marginLeft: 10, fontSize: 16,
-                        fontWeight: '800',
-                    }}>
-                        {name.replace(/_/g, ' ')}
-                    </Text>
-                    <Text style={{ color: 'grey', fontSize: 14, fontWeight: '200', marginLeft: 10 }}>
-                        Sent a friend request to you
-                        </Text>
+	const emptyStateScreen = () => {
+		return (
+			<View style={{ flex: 1 }}>
+				<Image
+					style={{ alignSelf: 'center', width: '70%', height: '60%', borderRadius: 20 }}
+					source={require('../../assets/EmptyFriendRequests.png')}
+				/>
+				<Text
+					style={{
+						textAlign: 'center',
+						justifyContent: 'center',
+						fontSize: 16,
+						fontWeight: 'bold',
+						marginTop: 20,
+					}}
+				>
+					No friend requests
+				</Text>
+				<Text
+					style={{
+						textAlign: 'center',
+						fontSize: 12,
+						color: '#A4A4A6',
+						fontWeight: '100',
+					}}
+				>
+					All your friend requests will be here
+				</Text>
+			</View>
+		);
+	};
 
-                    <View style={styles.buttonGroup}>
-                        <TouchableOpacity style={[styles.icon,
-                        {
-                            borderTopColor: '#7AB3EF', borderEndColor: '#6EE2E9', borderLeftColor: '#DED8DE',
-                            borderStartColor: '#D6A5D4', borderRightColor: '#DED8DE', borderBottomColor: '#4ACFEA'
-                        }]}
-                            onPress={() => acceptFriendRequest(name, userID, pictureURL)}>
-                            <Text>Accept</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={[styles.icon, {
-                            marginLeft: 20,
-                            borderTopColor: '#D6A5D4', borderEndColor: '#A49CF8', borderLeftColor: '#A49CF8',
-                            borderStartColor: '#D6A5D4', borderRightColor: '#DED8DE', borderBottomColor: '#D6A5D4'
-                        }]}
-                            onPress={() => rejectFriendRequest(name, userID)}>
-                            <Text>Decline</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View >
-        )
-    }
+	if (isLoading) {
+		return (
+			<View style={{ flex: 1, justifyContent: 'center' }}>
+				<ActivityIndicator size='large' />
+			</View>
+		);
+	}
 
-    const emptyStateScreen = () => {
-        return (
-            <View style={{ flex: 1 }}>
-                <Image
-                    style={{ alignSelf: 'center', width: '70%', height: '60%', borderRadius: 20 }}
-                    source={require("../../assets/EmptyFriendState.png")}
-                />
-                <Text style={{
-                    textAlign: 'center', justifyContent: 'center',
-                    fontSize: 16, fontWeight: 'bold',
-                    marginTop: 20,
-                }}>
-                    No friend requests
-                </Text>
-                <Text style={{
-                    textAlign: "center",
-                    fontSize: 12, color: '#A4A4A6', fontWeight: '100'
-                }}>
-                    All your friend requests will be here
-                </Text>
-            </View >
-        )
-    }
+	return (
+		<View style={styles.container}>
+			<View style={styles.header}>
+				<Text style={{ marginLeft: 20, fontSize: 20 }}> Friend Requests </Text>
+				<AntDesign
+					name='close'
+					size={24}
+					onPress={() => onClose()}
+					style={{ marginRight: 20, marginTop: 2 }}
+				/>
+			</View>
 
-    if (isLoading) {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center' }}>
-                <ActivityIndicator size='large' />
-            </View>
-        )
-    }
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={{ marginLeft: 20, fontSize: 20, }}> Friend Requests </Text>
-                <AntDesign
-                    name="close"
-                    size={24}
-                    onPress={() => onClose()}
-                    style={{ marginRight: 20, marginTop: 2 }}
-                />
-            </View>
-
-            <View style={styles.body}>
-                {friendRequests == 0
-                    ? emptyStateScreen()
-                    : null}
-                <View style={styles.friendRequests}>
-                    <SectionList
-                        progressViewOffset={100}
-                        sections={[
-                            { title: "", data: friendRequests },
-                        ]}
-                        renderItem={({ item }) => renderFriends(item[0], item[1], item[2])}
-                        keyExtractor={(item, index) => index}
-                    />
-                </View>
-            </View>
-
-        </View>
-    );
+			<View style={styles.body}>
+				{friendRequests == 0 ? emptyStateScreen() : null}
+				<View style={styles.friendRequests}>
+					<SectionList
+						progressViewOffset={100}
+						sections={[{ title: '', data: friendRequests }]}
+						renderItem={({ item }) => renderFriends(item[0], item[1], item[2])}
+						keyExtractor={(item, index) => index}
+					/>
+				</View>
+			</View>
+		</View>
+	);
 };
 
 const mapStateToProps = (state) => {
-    return {
-        userID: state.add_events.userID,
-        allFriends: state.add_friends.allFriends,
-        currUserProfilePicture: state.add_events.profilePicture
-    };
+	return {
+		userID: state.add_events.userID,
+		allFriends: state.add_friends.allFriends,
+		currUserProfilePicture: state.add_events.profilePicture,
+	};
 };
 
 const mapDispatchToProps = {
-    removeFriend, findFriends
+	removeFriend,
+	findFriends,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(FriendRequestModal);
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    header: {
-        flex: 1,
-        marginTop: '10%',
-        justifyContent: "space-between",
-        flexDirection: 'row'
-    },
-    headerButton: {
-        flex: 1,
-        justifyContent: "center",
-        alignSelf: 'flex-start',
-        borderRadius: 100,
-        paddingRight: 15,
-        paddingLeft: 15,
-        marginTop: 20,
-        marginBottom: 20,
-        marginLeft: '5%',
-        borderWidth: 1,
-    },
-    body: {
-        flex: 7,
-    },
-    friend: {
-        borderWidth: 0.5,
-        margin: 5,
-        borderColor: 'grey',
-        flexDirection: 'row',
-        alignSelf: 'center',
-        justifyContent: 'space-between',
-        height: 100,
-        width: '70%',
-        borderRadius: 8,
+	container: {
+		flex: 1,
+	},
+	header: {
+		flex: 1,
+		marginTop: '10%',
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+	},
+	headerButton: {
+		flex: 1,
+		justifyContent: 'center',
+		alignSelf: 'flex-start',
+		borderRadius: 100,
+		paddingRight: 15,
+		paddingLeft: 15,
+		marginTop: 20,
+		marginBottom: 20,
+		marginLeft: '5%',
+		borderWidth: 1,
+	},
+	body: {
+		flex: 7,
+	},
+	friend: {
+		borderWidth: 0.5,
+		margin: 5,
+		borderColor: 'grey',
+		flexDirection: 'row',
+		alignSelf: 'center',
+		justifyContent: 'space-between',
+		height: 100,
+		width: '70%',
+		borderRadius: 8,
+	},
+	buttonGroup: {
+		marginTop: 5,
+		flexDirection: 'row',
+		marginLeft: 10,
+	},
+	icon: {
+		borderWidth: 1,
+		padding: 5,
+		paddingLeft: 10,
+		paddingRight: 10,
+		borderRadius: 50,
+	},
+	title: {
+		fontSize: 32,
+	},
+	card: {
+		flexDirection: 'row',
+		borderWidth: 1,
+		justifyContent: 'flex-start',
+		margin: 10,
+		marginLeft: 20,
+		borderRadius: 10,
+		padding: 5,
+		width: Dimensions.get('window').width * 0.9,
+	},
+	addFriendButton: {
+		borderWidth: 0.5,
+		borderRadius: 5,
+		borderColor: 'black',
+		alignContent: 'stretch',
+	},
+	userName: {
+		fontSize: 18,
+		fontWeight: '800',
 
-    },
-    buttonGroup: {
-        marginTop: 5,
-        flexDirection: 'row',
-        marginLeft: 10,
-
-    },
-    icon: {
-        borderWidth: 1,
-        padding: 5,
-        paddingLeft: 10,
-        paddingRight: 10,
-        borderRadius: 50,
-
-    },
-    title: {
-        fontSize: 32,
-    },
-    card: {
-        flexDirection: 'row',
-        borderWidth: 1,
-        justifyContent: 'flex-start',
-        margin: 10,
-        marginLeft: 20,
-        borderRadius: 10,
-        padding: 5,
-        width: Dimensions.get('window').width * 0.9
-
-    },
-    addFriendButton: {
-        borderWidth: 0.5,
-        borderRadius: 5,
-        borderColor: 'black',
-        alignContent: 'stretch'
-
-    },
-    userName: {
-        fontSize: 18,
-        fontWeight: '800',
-
-        textAlign: 'center'
-
-    },
-    cardComponent: {
-        marginLeft: 10,
-        padding: 5,
-        width: Dimensions.get('window').width * 0.7,
-
-    },
-    addFriendButtonText: {
-        textAlign: 'center',
-    }
+		textAlign: 'center',
+	},
+	cardComponent: {
+		marginLeft: 10,
+		padding: 5,
+		width: Dimensions.get('window').width * 0.7,
+	},
+	addFriendButtonText: {
+		textAlign: 'center',
+	},
 });
