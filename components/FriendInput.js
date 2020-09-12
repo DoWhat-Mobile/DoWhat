823dc8c5480229657cbb8905fe2f25d3891f7c4b
hansebastian <sebastian_hans@hotmail.com>
2020-09-12 15:24:38 +0800

    Change padding of filter buttons to be consistent with DateSelection

--- components/FriendInput.js
+++ components/FriendInput.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { Image, View, Text, StyleSheet, Modal } from "react-native";
-import * as Linking from "expo-linking";
-import firebase from "../database/firebase";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { connect } from "react-redux";
-import { formatDateToString } from "../reusable-functions/GoogleCalendarGetBusyPeriods";
-import FriendsDisplay from "./FriendsDisplay";
+import React, { useEffect, useState } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Image, View, Text, StyleSheet, Modal } from 'react-native';
+import * as Linking from 'expo-linking';
+import firebase from '../database/firebase';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { connect } from 'react-redux';
+import { formatDateToString } from '../reusable-functions/GoogleCalendarGetBusyPeriods';
+import FriendsDisplay from './FriendsDisplay';
 
 /**
  * This component is a page for user to determine how many friends will be added to find the
@@ -14,269 +14,235 @@ import FriendsDisplay from "./FriendsDisplay";
  * User will only come to this page if and after snycing their Google Calendar.
  */
 const FriendInput = (props) => {
-    useEffect(() => {
-        addDatabaseToState();
-    }, []);
+	useEffect(() => {
+		addDatabaseToState();
+	}, []);
 
-    const [modalVisible, setModalVisible] = useState(false);
-    const [database, setDatabase] = useState({});
+	const [modalVisible, setModalVisible] = useState(false);
+	const [database, setDatabase] = useState({});
 
-    const addDatabaseToState = () => {
-        firebase
-            .database()
-            .ref()
-            .once("value")
-            .then((snapshot) => {
-                const database = snapshot.val();
-                setDatabase(database); // Add to component state for future checks if invitation alr sent
-            });
-    };
+	const addDatabaseToState = () => {
+		firebase
+			.database()
+			.ref()
+			.once('value')
+			.then((snapshot) => {
+				const database = snapshot.val();
+				setDatabase(database); // Add to component state for future checks if invitation alr sent
+			});
+	};
 
-    const closeModal = () => {
-        setModalVisible(false);
-    };
+	const closeModal = () => {
+		setModalVisible(false);
+	};
 
-    const shareWithTelegram = (url) => {
-        // Deep linking
-        Linking.openURL(
-            "https://t.me/share/url?url=" +
-                url +
-                "&text=" +
-                "\n" +
-                "Hey! Let's hang out! Use this link to input your availability!"
-        );
-    };
+	const shareWithTelegram = (url) => {
+		// Deep linking
+		Linking.openURL(
+			'https://t.me/share/url?url=' +
+				url +
+				'&text=' +
+				'\n' +
+				"Hey! Let's hang out! Use this link to input your availability!"
+		);
+	};
 
-    const shareWithWhatsapp = (url) => {
-        Linking.openURL(
-            "whatsapp://send?" +
-                "text=Hey! Let's hang out! Use this link to input your availability!" +
-                "\n" +
-                url
-        ).catch((err) => alert("Please download WhatsApp to use this feature"));
-    };
+	const shareWithWhatsapp = (url) => {
+		Linking.openURL(
+			'whatsapp://send?' +
+				"text=Hey! Let's hang out! Use this link to input your availability!" +
+				'\n' +
+				url
+		).catch((err) => alert('Please download WhatsApp to use this feature'));
+	};
 
-    const encodeUserInfoToURL = (url) => {
-        const userId = "#" + firebase.auth().currentUser.uid; // Add # for marking, so can extract from web-app
-        return url + encodeURIComponent(userId);
-    };
+	const encodeUserInfoToURL = (url) => {
+		const userId = '#' + firebase.auth().currentUser.uid; // Add # for marking, so can extract from web-app
+		return url + encodeURIComponent(userId);
+	};
 
-    // Hosted on AWS Amplify
-    const DoWhatWebURL = "https://master.da00s432t0f9l.amplifyapp.com/";
+	// Hosted on AWS Amplify
+	const DoWhatWebURL = 'https://master.da00s432t0f9l.amplifyapp.com/';
 
-    if (props.route.params.route == "collab") {
-        return (
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Text style={styles.titleText}>Invite your friends</Text>
-                </View>
+	if (props.route.params.route == 'collab') {
+		return (
+			<View style={styles.container}>
+				<View style={styles.header}>
+					<Text style={styles.titleText}>Invite your friends</Text>
+				</View>
 
-                <View style={styles.body}>
-                    <Image
-                        style={styles.image}
-                        source={require("../assets/FriendsHangout.png")}
-                    />
-                    <Text style={styles.subtitleText}>
-                        You have chosen your date and inputted your
-                        availabilities, now it's time to invite some of your
-                        friends to join you!
-                    </Text>
-                </View>
+				<View style={styles.body}>
+					<Image style={styles.image} source={require('../assets/FriendsHangout.png')} />
+					<Text style={styles.subtitleText}>
+						You have chosen your date and inputted your availabilities, now it's time to
+						invite some of your friends to join you!
+					</Text>
+				</View>
 
-                <View style={{ flex: 5, margin: 10 }}>
-                    <FriendsDisplay
-                        onClose={closeModal}
-                        database={database}
-                        navigation={props.navigation}
-                    />
-                </View>
+				<View style={{ flex: 5, margin: 10 }}>
+					<FriendsDisplay
+						onClose={closeModal}
+						database={database}
+						navigation={props.navigation}
+					/>
+				</View>
 
-                <View style={styles.footer}>
-                    <TouchableOpacity
-                        style={[
-                            styles.shareWithButton,
-                            {
-                                marginTop: 10,
-                                backgroundColor: "#cc5327",
-                                padding: 10,
-                                paddingLeft: 25,
-                                paddingRight: 25,
-                            },
-                        ]}
-                        onPress={() =>
-                            props.navigation.navigate("Plan", {
-                                addingFavourite: false,
-                            })
-                        }
-                    >
-                        <Text style={{ fontSize: 16, color: "white" }}>
-                            DONE
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        );
-    } else {
-        // Invite friends without DoWhat app
-        return (
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Text style={styles.titleText}>Invite your friends</Text>
-                </View>
-                <View style={styles.body}>
-                    <Image
-                        style={styles.image}
-                        source={require("../assets/FriendsHangout.png")}
-                    />
-                    <Text style={styles.subtitleText}>
-                        You have chosen your date and inputted your
-                        availabilities, now invite your friends to enter their
-                        availabilities as well!
-                    </Text>
+				<View style={styles.footer}>
+					<TouchableOpacity
+						style={[
+							styles.shareWithButton,
+							{
+								marginTop: 10,
+								backgroundColor: '#cc5327',
+								padding: 10,
+								paddingLeft: 25,
+								paddingRight: 25,
+							},
+						]}
+						onPress={() =>
+							props.navigation.navigate('Plan', {
+								addingFavourite: false,
+							})
+						}
+					>
+						<Text style={{ fontSize: 16, color: 'white' }}>DONE</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
+		);
+	} else {
+		// Invite friends without DoWhat app
+		return (
+			<View style={styles.container}>
+				<View style={styles.header}>
+					<Text style={styles.titleText}>Invite your friends</Text>
+				</View>
+				<View style={styles.body}>
+					<Image style={styles.image} source={require('../assets/FriendsHangout.png')} />
+					<Text style={styles.subtitleText}>
+						You have chosen your date and inputted your availabilities, now invite your
+						friends to enter their availabilities as well!
+					</Text>
 
-                    <View style={styles.shareButtons}>
-                        <TouchableOpacity
-                            style={[
-                                styles.shareWithButton,
-                                {
-                                    backgroundColor: "#0088CC",
-                                    paddingRight: 5,
-                                    paddingLeft: 5,
-                                },
-                            ]}
-                            onPress={() =>
-                                shareWithTelegram(
-                                    encodeUserInfoToURL(DoWhatWebURL)
-                                )
-                            }
-                        >
-                            <MaterialCommunityIcons
-                                name="send-circle"
-                                color={"white"}
-                                size={20}
-                            />
-                            <Text style={{ color: "white" }}>
-                                Telegram Invite
-                            </Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={[
-                                styles.shareWithButton,
-                                {
-                                    backgroundColor: "#25D366",
-                                    paddingRight: 5,
-                                    paddingLeft: 5,
-                                },
-                            ]}
-                            onPress={() =>
-                                shareWithWhatsapp(
-                                    encodeUserInfoToURL(DoWhatWebURL)
-                                )
-                            }
-                        >
-                            <MaterialCommunityIcons
-                                name="whatsapp"
-                                color={"white"}
-                                size={20}
-                            />
-                            <Text style={{ color: "white" }}>
-                                Whatsapp Invite
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
+					<View style={styles.shareButtons}>
+						<TouchableOpacity
+							style={[
+								styles.shareWithButton,
+								{
+									backgroundColor: '#0088CC',
+									paddingRight: 5,
+									paddingLeft: 5,
+								},
+							]}
+							onPress={() => shareWithTelegram(encodeUserInfoToURL(DoWhatWebURL))}
+						>
+							<MaterialCommunityIcons name='send-circle' color={'white'} size={20} />
+							<Text style={{ color: 'white' }}>Telegram Invite</Text>
+						</TouchableOpacity>
+						<TouchableOpacity
+							style={[
+								styles.shareWithButton,
+								{
+									backgroundColor: '#25D366',
+									paddingRight: 5,
+									paddingLeft: 5,
+								},
+							]}
+							onPress={() => shareWithWhatsapp(encodeUserInfoToURL(DoWhatWebURL))}
+						>
+							<MaterialCommunityIcons name='whatsapp' color={'white'} size={20} />
+							<Text style={{ color: 'white' }}>Whatsapp Invite</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
 
-                <View style={styles.footer}>
-                    <TouchableOpacity
-                        style={[
-                            styles.shareWithButton,
-                            {
-                                marginTop: 10,
-                                backgroundColor: "#cc5327",
-                                padding: 10,
-                                paddingLeft: 25,
-                                paddingRight: 25,
-                            },
-                        ]}
-                        onPress={() =>
-                            props.navigation.navigate("Loading", {
-                                route: "link",
-                                access: "host",
-                            })
-                        }
-                    >
-                        <Text style={{ fontSize: 16, color: "white" }}>
-                            Done Inviting
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        );
-    }
+				<View style={styles.footer}>
+					<TouchableOpacity
+						style={[
+							styles.shareWithButton,
+							{
+								marginTop: 10,
+								backgroundColor: '#cc5327',
+								padding: 10,
+								paddingLeft: 25,
+								paddingRight: 25,
+							},
+						]}
+						onPress={() =>
+							props.navigation.navigate('Loading', {
+								route: 'link',
+								access: 'host',
+							})
+						}
+					>
+						<Text style={{ fontSize: 16, color: 'white' }}>Done Inviting</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
+		);
+	}
 };
 
 const mapStateToProps = (state) => {
-    const dateInString = formatDateToString(state.date_select.date);
-    return {
-        userID: state.add_events.userID,
-        selected_date: dateInString,
-    };
+	const dateInString = formatDateToString(state.date_select.date);
+	return {
+		userID: state.add_events.userID,
+		selected_date: dateInString,
+	};
 };
 
 export default connect(mapStateToProps, null)(FriendInput);
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    header: {
-        flex: 1,
-        alignContent: "center",
-        alignItems: "center",
-        marginTop: "10%",
-    },
-    body: {
-        flex: 5,
-        alignContent: "center",
-        alignItems: "center",
-        marginTop: 0,
-        margin: "5%",
-    },
-    footer: {
-        flex: 2,
-        flexDirection: "column",
-        alignContent: "center",
-        alignItems: "center",
-        justifyContent: "space-around",
-    },
-    titleText: {
-        fontSize: 25,
-        fontWeight: "600",
-    },
-    image: {
-        width: 200,
-        height: 200,
-        borderRadius: 100,
-        borderWidth: 0.5,
-        borderColor: "black",
-    },
-    subtitleText: {
-        fontSize: 12,
-        color: "grey",
-        textAlign: "center",
-        marginTop: 20,
-    },
-    shareButtons: {
-        flexDirection: "row",
-        marginTop: 20,
-        justifyContent: "space-around",
-        width: "100%",
-    },
-    shareWithButton: {
-        borderRadius: 5,
-        borderWidth: 0.5,
-        borderColor: "black",
-        padding: 4,
-        flexDirection: "row",
-    },
+	container: {
+		flex: 1,
+	},
+	header: {
+		flex: 1,
+		alignContent: 'center',
+		alignItems: 'center',
+		marginTop: '10%',
+	},
+	body: {
+		flex: 5,
+		alignContent: 'center',
+		alignItems: 'center',
+		marginTop: 0,
+		margin: '5%',
+	},
+	footer: {
+		flex: 2,
+		flexDirection: 'column',
+		alignContent: 'center',
+		alignItems: 'center',
+		justifyContent: 'space-around',
+	},
+	titleText: {
+		fontSize: 25,
+		fontWeight: '600',
+	},
+	image: {
+		width: 200,
+		height: 200,
+		borderRadius: 100,
+		borderWidth: 0.5,
+		borderColor: 'black',
+	},
+	subtitleText: {
+		fontSize: 12,
+		color: 'grey',
+		textAlign: 'center',
+		marginTop: 20,
+	},
+	shareButtons: {
+		flexDirection: 'row',
+		marginTop: 20,
+		justifyContent: 'space-around',
+		width: '100%',
+	},
+	shareWithButton: {
+		borderRadius: 5,
+		padding: 4,
+		flexDirection: 'row',
+	},
 });
--- components/genre/AreaSelection.js
+++ components/genre/AreaSelection.js
@@ -1,50 +1,48 @@
-import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { buttons } from "../../reusable-functions/buttons";
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { buttons } from '../../reusable-functions/buttons';
 const AreaSelection = ({ handleAreaPress }) => {
-    const [selected, setSelected] = React.useState([]);
-    const areas = ["North", "East", "West", "Central"];
+	const [selected, setSelected] = React.useState([]);
+	const areas = ['North', 'East', 'West', 'Central'];
 
-    const handlePress = (area) => {
-        let newSelected = [];
+	const handlePress = (area) => {
+		let newSelected = [];
 
-        if (selected.includes(area)) {
-            newSelected = selected.filter((s) => s !== area);
-        } else {
-            newSelected = selected.concat(area);
-        }
+		if (selected.includes(area)) {
+			newSelected = selected.filter((s) => s !== area);
+		} else {
+			newSelected = selected.concat(area);
+		}
 
-        setSelected(newSelected);
-        handleAreaPress(newSelected);
-    };
+		setSelected(newSelected);
+		handleAreaPress(newSelected);
+	};
 
-    return (
-        <View style={{ marginTop: 20 }}>
-            <Text style={styles.header}>Areas</Text>
-            <View style={styles.buttonContainer}>
-                {buttons(areas, selected, handlePress)}
-            </View>
-        </View>
-    );
+	return (
+		<View style={{ marginTop: 20 }}>
+			<Text style={styles.header}>Areas</Text>
+			<View style={styles.buttonContainer}>{buttons(areas, selected, handlePress)}</View>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-    button: {
-        borderRadius: 10,
-        padding: 5,
-        borderWidth: StyleSheet.hairlineWidth,
-        marginHorizontal: 5,
-        marginVertical: 10,
-    },
-    buttonContainer: {
-        flexDirection: "row",
-        flexWrap: "wrap",
-    },
-    header: {
-        fontSize: 20,
-        fontWeight: "bold",
-        marginLeft: 5,
-    },
+	button: {
+		borderRadius: 2,
+		padding: 5,
+		borderWidth: StyleSheet.hairlineWidth,
+		marginHorizontal: 5,
+		marginVertical: 10,
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+	},
+	header: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		marginLeft: 5,
+	},
 });
 
 export default AreaSelection;
